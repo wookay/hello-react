5e069c59196862ea19d15eceeeb771a3f4933619
WooKyoung Noh <wookay.noh@gmail.com>
2021-06-23 09:41:09 +0900

    more tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,5 +1,10 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen} from '@testing-library/react';
 import App from './App';
+import renderer from 'react-test-renderer';
+
+function Button(props) {
+  return <button>Nothing to do for now</button>;
+}
 
 describe('describe renders', function() {
   it('renders learn react link', () => {
@@ -10,16 +15,36 @@ describe('describe renders', function() {
 
   it('renders 하더놈', () => {
     render(<App />);
-    const linkElement = screen.getByText(/하더놈/i);
-    expect(linkElement).toBeInTheDocument();
+    const p = screen.getByText("하더놈", { exact: false });
+    expect(p).toBeInTheDocument();
+  });
+
+  it('unmount', () => {
+    const { container, unmount } = render(<App />);
+    unmount();
+    var got_error = false
+    try {
+      const p = screen.getByText("하더놈", { exact: false });
+    } catch (error) {
+      expect(error.name == "TestingLibraryElementError").toBe(true);
+      got_error = true
+    }
+    expect(got_error).toBe(true);
+  });
+
+  it('renders toJSON', () => {
+    const button = renderer.create(<Button />);
+    expect(button.toJSON()).toEqual({ type: 'button', props: {}, children: [ 'Nothing to do for now' ] });
   });
 }); // describe renders
 
-describe('describe toEqual', function() {
-  it('it', () => {
-    expect(3 == 1+2).toEqual(true);
+
+describe('describe equals', function() {
+  it('==, !=', () => {
+    expect(3 == 1+2).toBe(true);
+    expect(3 != 1).toBe(true);
   });
-}); // describe toEqual
+}); // describe equals
 
 /*
 https://github.com/testing-library/jest-dom
